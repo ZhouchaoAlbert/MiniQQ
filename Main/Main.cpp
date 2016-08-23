2a9a4e99095405dc269db2daa9ce0a53136f6c12
ZhouchaoAlbert <838944042@qq.com>
2016-08-24 01:05:50 +0800

    111

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,11 +1,14 @@
-#include <windows.h>
+#include<Windows.h>
+#include "UtilPath.h"
 
+#include <atlstr.h>
+using namespace ATL;
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
+int APIENTRY  WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	
 
-
+	//CString strPath = Util::Path::GetAppPath();
 
 
 	::CoUninitialize();
