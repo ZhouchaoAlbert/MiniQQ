b9cdc5af52029338796cc4192f913b5c6592bd85
ZhouChaoAlbert <838944042@qq.com>
2016-08-23 19:22:04 +0800

    Test

---
+++ Main/Main.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
+{
+	
+
+
+
+
+	::CoUninitialize();
+	return 0;
+}
