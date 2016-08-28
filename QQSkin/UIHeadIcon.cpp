90bdea94ff27bc7ac749d7fa7f1c363349eab79c
ZhouchaoAlbert <838944042@qq.com>
2016-08-29 00:23:49 +0800

    关闭最小化
    Former-commit-id: 94a9ed89a14f64920de04ab96333b615387bb611

---
+++ QQSkin/UIHeadIcon.cpp
@@ -0,0 +1,87 @@
+#include "stdafx.h"
+#include <atlimage.h>
+#include "UIHeadIcon.h"
+
+namespace DuiLib{
+
+CUIHeadIcon::CUIHeadIcon()
+{
+    m_nCircleBorderWidth = 0;
+}
+
+CUIHeadIcon::~CUIHeadIcon()
+{
+}
+
+LPCTSTR CUIHeadIcon::GetClass() const
+{
+    return _T("HeadIconUI");
+}
+
+	LPVOID CUIHeadIcon::GetInterface(LPCTSTR pstrName)
+{
+	if (_tcscmp(pstrName, _T("HeadIconUI")) == 0) return static_cast<CUIHeadIcon*>(this);
+    return __super::GetInterface(pstrName);
+}
+
+void CUIHeadIcon::SetAttribute(LPCTSTR pstrName, LPCTSTR pstrValue)
+{
+    if (_tcscmp(pstrName, _T("circleborderwidth")) == 0) SetCircleBorderWidth(_ttoi(pstrValue));
+    else __super::SetAttribute(pstrName, pstrValue);
+}
+
+void CUIHeadIcon::PaintBkImage(HDC hDC)
+{
+    __super::PaintBkImage(hDC);
+
+	UINT32 iW = m_rcItem.right - m_rcItem.left;
+	UINT32 iH = m_rcItem.bottom - m_rcItem.top;
+	UINT32 nWidth	= iW - 2 * m_nCircleBorderWidth;
+	UINT32 nHeight  = iH - 2 * m_nCircleBorderWidth;
+
+	HBITMAP hBmp = ::CreateCompatibleBitmap(hDC, nWidth, nHeight);
+	HDC hMemDC = CreateCompatibleDC(hDC);
+	HBITMAP hOriBmp = (HBITMAP)SelectObject(hMemDC, hBmp);
+	RECT rcBmp = { 0, 0, nWidth, nHeight };
+	::SetBkColor(hMemDC, RGB(255, 255, 255));
+	::ExtTextOut(hMemDC, 0, 0, ETO_OPAQUE, &rcBmp, NULL, 0, NULL);
+	CRenderEngine::DrawImageString(hMemDC, GetManager(), rcBmp, rcBmp, m_strUserIcon);
+	Gdiplus::Bitmap bitmap(hBmp, NULL);
+	SelectObject(hMemDC, hOriBmp);
+	DeleteDC(hMemDC);
+
+	//创建圆形图片
+	Gdiplus::Bitmap bmpRound(nWidth, nHeight);
+	Gdiplus::Graphics g2(&bmpRound);
+	Gdiplus::RectF	rcDest((Gdiplus::REAL)0, (Gdiplus::REAL)0, (Gdiplus::REAL)nWidth, (Gdiplus::REAL)nHeight);
+	Gdiplus::TextureBrush brush(&bitmap, rcDest);
+	g2.SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);
+	g2.FillEllipse(&brush, 0, 0, nWidth - 1, nHeight - 1);
+
+	DeleteObject(hBmp);
+
+	//把圆形图片贴到窗口上
+	Gdiplus::Graphics	g(hDC);
+	UINT32 nX = m_rcItem.left + m_nCircleBorderWidth;
+	UINT32 nY = m_rcItem.top + m_nCircleBorderWidth;
+#if 0
+	Gdiplus::CachedBitmap cachedBitmap(&bmpRound, &g);
+	g.DrawCachedBitmap(&cachedBitmap, nX, nY);
+#else
+	g.DrawImage(&bmpRound, Gdiplus::Rect(nX, nY, nWidth, nHeight));
+#endif
+}
+
+void CUIHeadIcon::SetCircleBorderWidth(UINT nWidth)
+{
+    m_nCircleBorderWidth = nWidth;
+    Invalidate();
+}
+
+void CUIHeadIcon::SetHeadIcon(LPCTSTR szIcon)
+{
+	m_strUserIcon = szIcon;
+    Invalidate();
+}
+
+}
---
+++ QQSkin/UtilPath.cpp
@@ -0,0 +1,269 @@
+#include "stdafx.h"
+#include "UtilPath.h"
+
+
+CString Util::Path::GetAppPath()
+{
+	TCHAR szPath[MAX_PATH] = { 0 };
+	GetModuleFileName(NULL, szPath, MAX_PATH);
+	CString strPath(szPath);
+	INT32 iFind = strPath.ReverseFind(_T('\\'));
+	if (iFind < 0)
+	{
+		return _T("");
+	}
+	strPath = strPath.Left(iFind);
+	return strPath;
+}
+
+CString Util::Path::GetCurrentDir()
+{
+	TCHAR szCurDir[MAX_PATH] = { 0 };
+	::GetCurrentDirectory(MAX_PATH, szCurDir);
+
+	UINT32 uLen = _tcslen(szCurDir);
+	if (uLen <= 0)
+	{
+		return _T("");
+	}
+	TCHAR cLastChar = szCurDir[uLen - 1];
+
+	if (cLastChar != _T('\\') && cLastChar != _T('/'))
+	{
+		_tcscat(szCurDir, _T("\\"));
+	}
+	return szCurDir;
+}
+
+CString  Util::Path::GetTempPath()
+{
+	TCHAR szTempPath[MAX_PATH] = { 0 };
+	::GetTempPath(MAX_PATH, szTempPath);
+
+	UINT32 uLen = _tcslen(szTempPath);
+	if (uLen <= 0)
+	{
+		return _T("");
+	}
+	
+	TCHAR cLastChar = szTempPath[uLen - 1];
+	if (cLastChar != _T('\\') && cLastChar != _T('/'))
+	{
+		_tcscat(szTempPath, _T("\\"));
+	}
+	return szTempPath;
+}
+
+
+CString Util::Path::GetRandomFileName(LPCTSTR lpszPath, LPCTSTR lpszFileName)
+{
+	CString strPath, strFileName, strExtFileName, strFullPath;
+	TCHAR szBuf[MAX_PATH] = { 0 };
+
+	if (IsDirectoryExist(lpszPath))
+	{
+		strPath = lpszPath;
+	}	
+	else
+	{
+		strPath = GetAppPath();	
+	}
+
+	strFileName	   = GetFileNameWithoutExtension(lpszFileName);
+	strExtFileName = GetExtension(lpszFileName);
+
+	for (INT32 i = 2; i < 10000; i++)
+	{
+		if (strExtFileName.IsEmpty())
+		{
+			strFullPath = strPath;
+			strFullPath += strFileName;
+			wsprintf(szBuf, _T("%d"), i);
+			strFullPath += szBuf;
+		}
+		else
+		{
+			strFullPath = strPath;
+			strFullPath += strFileName;
+			wsprintf(szBuf, _T("%d."), i);
+			strFullPath += szBuf;
+			strFullPath += strExtFileName;
+		}
+
+		if (!IsFileExist(strFullPath))
+		{
+			return strFullPath;
+		}
+	}
+	return _T("");
+}
+
+CString Util::Path::GetTempFileName(LPCTSTR lpszFileName)
+{
+	return GetRandomFileName(GetTempPath(), lpszFileName);
+}
+
+CString  Util::Path::GetInstallFolder()    //获取安装目录
+{
+	CString strPath = GetAppPath();
+	ATLASSERT(!strPath.IsEmpty());
+
+	INT32 ix = strPath.ReverseFind(_T('\\'));
+	if (ix != -1)
+	{
+		return strPath.Mid(0, ix);
+	}
+	ATLASSERT(FALSE);
+	return _T("");
+}
+
+
+
+CString Util::Path::GetResFolder()
+{
+	return GetInstallFolder() + _T("\\Resource");
+}
+
+CString Util::Path::GetResMaterialFolder()
+{
+	return GetResFolder() + _T("\\material");
+}
+
+CString Util::Path::GetResXmlFolder()
+{
+	return GetResFolder() + _T("\\xml");
+}
+
+BOOL Util::Path::IsDirectory(LPCTSTR lpszPath)
+{
+	if (NULL == lpszPath || NULL == *lpszPath)
+	{
+		return FALSE;
+	}
+
+	DWORD dwAttr = ::GetFileAttributes(lpszPath);
+	return (((dwAttr != 0xFFFFFFFF) && (dwAttr & FILE_ATTRIBUTE_DIRECTORY)) ? TRUE : FALSE);
+}
+
+BOOL Util::Path::IsFileExist(LPCTSTR lpszFileName)
+{
+	if (NULL == lpszFileName || NULL == *lpszFileName)
+	{
+		return FALSE;
+	}
+	
+	DWORD dwAttr = ::GetFileAttributes(lpszFileName);
+	return (((dwAttr != 0xFFFFFFFF) && (!(dwAttr & FILE_ATTRIBUTE_DIRECTORY))) ? TRUE : FALSE);
+}
+
+BOOL Util::Path::IsDirectoryExist(LPCTSTR lpszPath)
+{
+	if (NULL == lpszPath || NULL == *lpszPath)
+	{
+		return FALSE;
+	}
+		
+	DWORD dwAttr = ::GetFileAttributes(lpszPath);
+	return (((dwAttr != 0xFFFFFFFF) && (dwAttr & FILE_ATTRIBUTE_DIRECTORY)) ? TRUE : FALSE);
+}
+
+BOOL Util::Path::CreateDirectory(LPCTSTR lpPathName, LPSECURITY_ATTRIBUTES lpSecurityAttributes)
+{
+	if (NULL == lpPathName || NULL == *lpPathName)
+	{
+		return FALSE;
+	}
+	TCHAR cPath[MAX_PATH]	 = { 0 };
+	TCHAR cTmpPath[MAX_PATH] = { 0 };
+	TCHAR *lpPos = NULL;
+	TCHAR  cTmp  = _T('\0');
+
+	_tcsncpy(cPath, lpPathName, MAX_PATH);
+
+	for (INT32 i = 0; i < (INT32)_tcslen(cPath); i++)
+	{
+		if (_T('\\') == cPath[i])
+		{
+			cPath[i] = _T('/');
+		}
+	}
+
+	lpPos = _tcschr(cPath, _T('/'));
+	while (NULL != lpPos)
+	{
+		if (lpPos == cPath)
+		{
+			lpPos++;
+		}
+		else
+		{
+			cTmp = *lpPos;
+			*lpPos = _T('\0');
+			_tcsncpy(cTmpPath, cPath, MAX_PATH);
+			::CreateDirectory(cTmpPath, lpSecurityAttributes);
+			*lpPos = cTmp;
+			lpPos++;
+		}
+		lpPos = _tcschr(lpPos, _T('/'));
+	}
+	return TRUE;
+}
+
+CString Util::Path::GetPathRoot(LPCTSTR lpszPath)
+{
+	if (NULL == lpszPath || NULL == *lpszPath)
+	{
+		return _T("");
+	}
+
+	CString strPath(lpszPath);
+
+
+	return strPath;
+}
+
+CString Util::Path::GetExtension(LPCTSTR lpszPath)
+{
+	if (NULL == lpszPath || NULL == *lpszPath)
+	{
+		return _T("");
+	}
+	CString strPath(lpszPath);
+	INT32 iFind = strPath.ReverseFind('\\');
+	if (iFind < 0)
+	{
+		iFind = strPath.ReverseFind('/');
+	}
+	CString strFileName = strPath.Right(strPath.GetLength() - 1 - iFind);
+
+	iFind = strFileName.ReverseFind('.');
+	if (iFind < 0)
+	{
+		return _T("");
+	}
+	strFileName = strFileName.Right(strFileName.GetLength() - 1- iFind);
+	return strFileName;
+}
+
+CString Util::Path::GetFileNameWithoutExtension(LPCTSTR lpszPath)
+{
+	if (NULL == lpszPath || NULL == *lpszPath)
+	{
+		return _T("");
+	}
+	CString strPath(lpszPath);
+	INT32 iFind = strPath.ReverseFind('\\');
+	if (iFind < 0)
+	{
+		iFind = strPath.ReverseFind('/');
+	}
+	CString strFileName = strPath.Right(strPath.GetLength() - 1 - iFind);
+	
+	iFind = strFileName.ReverseFind('.');
+	if (iFind < 0)
+	{
+		return _T("");
+	}
+	strFileName = strFileName.Left(iFind);
+	return strFileName;
+}
